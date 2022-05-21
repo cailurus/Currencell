492a73a6d2b0e8be98e109478171a2b6f9ed7fb0
jinyzho <jinyzho@microsoft.com>
2022-05-21 13:58:06 +0000

    added clipped df

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def parameter_panel(smoother_name):
 
     elif smoother_name == "Convolution Smoother":
         with col2:
-            window_len = st.number_input(label="Window Length", min_value=1, max_value=100, vlaue=20)
+            window_len = st.number_input(label="Window Length", min_value=1, max_value=100, value=20)
         with col3:
             window_type = st.selectbox(
                 label="Window Type", options=["ones", "hanning", "hamming", "bartlett", "blackman"], value="hanning"
@@ -52,75 +52,111 @@ def smooth_process(data, smoother_meta):
     smoothed_df = pd.DataFrame(
         {
             "Elapsed Time (s)": data["Elapsed Time (s)"][-len(smoothed_value) :],
-            "Smoothed Current (A)": smoothed_value,
             "Current (A)": original_value,
+            "Smoothed Current (A)": smoothed_value,
         }
     )
     return smoothed_df
 
 
 st.title("Currencell")
-st.markdown("What kind of wonders happen when electricity passes through a cell?")
+st.markdown(
+    "After the cell is stretched and hydrogen peroxide is released, how can the released bioelectricity be measured?"
+)
 
+# Upload file
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     default_file = uploaded_file
 else:
     default_file = "./data.txt"
 
+
 st.header("Smoother Setup")
 col1, col2, col3 = st.columns(3)
-
 with col1:
     smoother_name = st.selectbox(
         label="Smoother", options=["Exponential Smoother", "Spectral Smoother", "Convolution Smoother"], index=1
     )
+smoother_meta = parameter_panel(smoother_name)  # col2 and col3
 
-## col2 and col3
-smoother_meta = parameter_panel(smoother_name)
 
-data = pd.read_csv(default_file, header=0, sep="\t", usecols=["Elapsed Time (s)", "Current (A)"])[6000:]
-smoothed_data = smooth_process(data, smoother_meta)
-
-st.header("Selection Setup")
-# Using the "with" syntax
-with st.form(key="Intervals"):
+st.header("Intervals & Baseline Setup")
+with st.form(key="baseline panel"):
     ccc0, ccc1 = st.columns(2)
     with ccc0:
         intervals_string = st.text_input(
-            label="Enter the intervals", placeholder="Please input the intervals. (100, 150), (500, 600)"
+            label="Enter the intervals",
+            placeholder="Please input the intervals. (100, 150), (500, 600)",
+            value="(1200,1400), (3000,3100)",
         )
     with ccc1:
         baseline_model_name = st.selectbox(label="Baseline Model", options=["Polynomial"], index=0)
 
-    intervals_submitted = st.form_submit_button(label="Submit")
+    baseline_setup_submitted = st.form_submit_button(label="Submit")
 
 
-fig = px.line(smoothed_data, x="Elapsed Time (s)", y=["Current (A)", "Smoothed Current (A)"])
+data = pd.read_csv(default_file, header=0, sep="\t", usecols=["Elapsed Time (s)", "Current (A)"])[6000:-5000]
+st.header("Visualization")
+fig = px.line(data, x="Elapsed Time (s)", y=[_ for _ in data.columns if _ != "Elapsed Time (s)"])
+fig.update_layout(showlegend=True)
 
-if intervals_submitted:
-    intervals_string = intervals_string.replace("(", " ").replace(")", " ").replace(",", " ")
+
+def interval_process(raw_string):
+    intervals_string = raw_string.replace("(", " ").replace(")", " ").replace(",", " ")
     intervals = [float(_) for _ in intervals_string.split()]
-
+    paired_intervals = []
     for index in range(len(intervals) // 2):
-        baseline_input_data = smoothed_data.drop(
-            smoothed_data.index[int(intervals[index * 2]) : int(intervals[index * 2 + 1])]
-        )
+        paired_intervals.append((intervals[index * 2], intervals[index * 2 + 1]))
+
+    return paired_intervals
+
+
+smoothed_data = smooth_process(data, smoother_meta)
+fig = px.line(smoothed_data, x="Elapsed Time (s)", y=[_ for _ in smoothed_data.columns if _ != "Elapsed Time (s)"])
+
+
+if baseline_setup_submitted:
+    intervals = []
+    intervals = interval_process(intervals_string)
+
+    for pair in intervals:
+        baseline_input_data = smoothed_data.drop(smoothed_data.index[int(pair[0]) : int(pair[1])])
 
     poly = np.polyfit(x=baseline_input_data["Elapsed Time (s)"], y=baseline_input_data["Smoothed Current (A)"], deg=3)
-    baseline_value = np.polyval(poly, smoothed_data["Elapsed Time (s)"])
+    baseline_value = np.polyval(poly, data["Elapsed Time (s)"])
 
     smoothed_data["Baseline"] = baseline_value
 
     fig = px.line(smoothed_data, x="Elapsed Time (s)", y=["Current (A)", "Smoothed Current (A)", "Baseline"])
-    for index in range(len(intervals) // 2):
-        fig.add_vrect(x0=intervals[index * 2], x1=intervals[index * 2 + 1], line_width=0, fillcolor="red", opacity=0.2)
+    for pair in intervals:
+        fig.add_vrect(x0=pair[0], x1=pair[1], line_width=0, fillcolor="red", opacity=0.2)
 
+    clipped_dfs = []
 
-fig.update_layout(
-    showlegend=True,
-)
+    for pair in intervals:
+        clipped_dfs.append(
+            smoothed_data[
+                (smoothed_data["Elapsed Time (s)"] > (pair[0] - 50))
+                & (smoothed_data["Elapsed Time (s)"] < (pair[1] + 50))
+            ]
+        )
+
+    col1, col2 = st.columns(2)
+    with col1:
+        clip_fig_1 = px.line(
+            clipped_dfs[0],
+            x="Elapsed Time (s)",
+            y=[_ for _ in smoothed_data.columns if _ != "Elapsed Time (s)"],
+        )
+        clip_fig_1.update_layout(showlegend=False)
+        st.plotly_chart(clip_fig_1, use_container_width=True)
+    with col2:
+        clip_fig_2 = px.line(
+            clipped_dfs[1], x="Elapsed Time (s)", y=[_ for _ in smoothed_data.columns if _ != "Elapsed Time (s)"]
+        )
+        clip_fig_2.update_layout(showlegend=False)
+        st.plotly_chart(clip_fig_2, use_container_width=True)
+
 
 st.plotly_chart(fig, use_container_width=True)
-
-# st.table(data.head(20).style.format({"Current (A)": "{:.8f}", "Elapsed Time (s)": "{:.2f}"}))
